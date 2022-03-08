e94263f7065bec7d7370cd306d31a38c93d163a7
ArteMaxL <artemiolucero@gmail.com>
2022-03-08 01:59:07 -0300

    Added header and directives

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+})
+export class DirectivaComponent {
+  listaProgram: string[] = [
+    'TypeScript',
+    'JavaScript',
+    'Java SE',
+    'C#',
+    'Python',
+  ];
+
+  habilitar: boolean = true;
+
+  constructor() {}
+
+  setHabilitar(): void {
+    this.habilitar = this.habilitar == true ? false : true;
+  }
+}
